957e5156a81d183c44a904c4673f7e7f7f4ce7d5
john alexander agudelo <johnaagudelo@gmail.com>
2020-04-11 12:15:24 -0500

    exercise of this

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,12 @@ const video = document.querySelector('video')
 const button = document.querySelector('#playButton')
 const muteButton = document.querySelector('#muteButton');
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()]});
+const player = new MediaPlayer({
+  el: video,
+  plugins: [
+    // new AutoPlay()
+  ]
+});
 button.onclick = () => { player.tooglePlay() }
 muteButton.onclick = () => {
   if (player.media.muted) {
