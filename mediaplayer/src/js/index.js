4c3f9d3a09fce9c9d34db9e68b5b5f10d720fba6
john alexander agudelo <johnaagudelo@gmail.com>
2020-05-05 23:41:01 -0500

    setup react and webpack

---
+++ mediaplayer/src/js/index.js
@@ -0,0 +1,26 @@
+import MediaPlayer from "./MediaPlayer"
+import AutoPlay from './plugins/AutoPlay'
+import AutoPause from './plugins/AutoPause'
+
+const video = document.querySelector('video')
+const button = document.querySelector('#playButton')
+const muteButton = document.querySelector('#muteButton');
+
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
+button.onclick = () => { player.tooglePlay() }
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute()
+    return
+  }
+  player.mute()
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').catch(error => {
+    console.log(error.message)
+  })
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+}
